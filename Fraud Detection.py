05700472bc6064fc24658c03f84785b01efd03fd
Max-Ola <106966289+Max-Ola@users.noreply.github.com>
2022-12-21 11:40:50 +0300

    Add files via upload

---
+++ Fraud Detection.py
@@ -0,0 +1,34 @@
+# Import necessary libraries
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix, classification_report
+
+# Load the credit card transactions data
+data = pd.read_csv('credit_card_transactions.csv')
+
+# Split the data into features and target
+X = data.drop('is_fraud', axis=1)
+y = data['is_fraud']
+
+# Split the data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Scale the features
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+# Build the model
+model = RandomForestClassifier(n_estimators=100, random_state=42)
+
+# Train the model on the training data
+model.fit(X_train, y_train)
+
+# Make predictions on the test data
+y_pred = model.predict(X_test)
+
+# Evaluate the model's performance
+print(confusion_matrix(y_test, y_pred))
+print(classification_report(y_test, y_pred))
